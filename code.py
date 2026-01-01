0c1cfa490e769d681342488247ec3c0a776c5175
Surjanthakur <tsurjan506@gmail.com>
2026-01-01 23:01:58 +0530

    save

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
-age = 18
+num = 5
 
-if age < 13:
-    print("child")
-elif age > 13 and age < 19:
-    print("teenage")
-elif age > 20 and age < 59:
-    print("adult")
-else:
-    print("senior")
+for i in range(num, num * 11, num):
+    if i == 25:
+        continue
+    print(i)
