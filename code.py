75049933221cfce5e971b99ce62be3b567903b02
Surjanthakur <tsurjan506@gmail.com>
2026-01-01 18:01:24 +0530

    svae

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
-cars = ("lambo", "buggati", "mahindra")
+age = 18
+
+if age < 13:
+    print("child")
+elif age > 13 and age < 19:
+    print("teenage")
+elif age > 20 and age < 59:
+    print("adult")
+else:
+    print("senior")
