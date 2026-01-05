c0ff0a1131e49669178de1c971a179897d549409
Surjanthakur <tsurjan506@gmail.com>
2026-01-05 20:57:07 +0530

    save

--- s.py
+++ s.py
@@ -1,28 +1,12 @@
-class AtmMachine:
-    def __init__(self):
-        self.pin = ""
-        self.balance = 0
+class Fraction:
+    def __init__(self, n1, n2):
+        self.num1 = n1
+        self.num2 = n2
 
-    def menu(self):
-        user_input = input(
-            """ 
-                           hello would you like to process?
-                           press 1 to create pin
-                           press 2 to view balance
-                           press 3 to add balance
-                           press 4 to exit
-                           """
-        )
-        if user_input == "1":
-            print("create pin")
-        elif user_input == "2":
-            print("view balance")
-        elif user_input == "3":
-            print("add balance")
-        elif user_input == "4":
-            print("exit")
+    def addition(self):
+        print(self.num1 + self.num2)
 
 
-sbi = AtmMachine()
+x = Fraction(4, 5)
 
-print(sbi.menu())
+x.addition()
