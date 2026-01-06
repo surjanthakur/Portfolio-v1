643d9d5485c4eea3441c36bf603f1ca8a739616c
Surjanthakur <tsurjan506@gmail.com>
2026-01-06 11:02:51 +0530

    save

--- s.py
+++ s.py
@@ -1,12 +1,12 @@
-class Fraction:
-    def __init__(self, n1, n2):
-        self.num1 = n1
-        self.num2 = n2
+class Fraction:  # class
+    def __init__(self, n1, n2):  # constructor
+        self.__num1 = n1
+        self.__num2 = n2
 
-    def addition(self):
-        print(self.num1 + self.num2)
+    def addition(self):  # method
+        return self.__num1 * self.__num2
 
 
-x = Fraction(4, 5)
+x = Fraction(4, 5)  # object. => now this object is self
 
-x.addition()
+print(x.addition())
